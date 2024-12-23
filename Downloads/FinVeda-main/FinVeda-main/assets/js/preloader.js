57281405c16a7f5f9da8976d32b73bce5f200864
Ketanop321 <146806880+Ketanop321@users.noreply.github.com>
2024-12-23 23:32:55 +0530

    Delete Downloads directory

--- Downloads/FinVeda-main/FinVeda-main/assets/js/preloader.js
+++
@@ -1,9 +0,0 @@
-function hidePreloader() {
-    var preloader = document.getElementById('preloader');
-    if (preloader) {
-        preloader.style.display = 'none';
-        document.body.style.overflowY = 'auto';
-    }
-}
-
-setTimeout(hidePreloader, 3000);
